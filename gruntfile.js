345cf6273d984535de017544921c596ef4ee69d1
DQ_Sujasree <sujasree.kurapati@deque.com>
2018-08-24 14:41:40 +0530

    more changes to test command line command

--- gruntfile.js
+++ gruntfile.js
@@ -1,16 +1,16 @@
 
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        unittesting : {
-            unit : {
+        //pkg: grunt.file.readJSON('package.json'),
+        unittesting: {
+          //  unit: {
                 src: ['test/unit.js']
-            }
+           // }
         }
     });
 
-    //grunt.loadNpmTasks('grunt-contrib-unittesting');
-    //grunt.registerTask('test', ['unittesting:unit']);
+   // grunt.loadTasks('grunt-unittesting');
+   // grunt.registerTask('test', ['unittesting']);
 
     grunt.registerTask('test', 'A sample task that logs stuff.', function(arg1, arg2) {
         if (arguments.length === 0) {
@@ -18,5 +18,5 @@ module.exports = function (grunt) {
         } else {
           grunt.log.writeln(this.name + ", " + arg1 + " " + arg2);
         }
-      });
+      }); 
 }
